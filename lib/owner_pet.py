834fa66e8c213ee93f203a64cd1fb9cf3ea1d784
nandabi <valerie.nandabi@student.moringaschool.com>
2024-03-18 14:11:45 +0300

    commit message

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,40 +1,30 @@
 class Pet:
-    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
-    all_pets = []
 
-    def __init__(self, name, pet_type, owner=None):
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner = None):
+        if pet_type not in self.PET_TYPES:
+            raise Exception("Invalid pet type")
+        
         self.name = name
-        if pet_type.lower() not in self.PET_TYPES:
-            raise ValueError(f"Invalid pet type '{pet_type}'. Must be one of: {', '.join(self.PET_TYPES)}")
-        self.pet_type = pet_type.lower()
-        if owner is not None:
-            if not isinstance(owner, Owner):
-                raise TypeError("Owner must be an instance of the Owner class.")
+        self.pet_type = pet_type
         self.owner = owner
-        self.all_pets.append(self)
+
+        Pet.all.append(self)
 
 class Owner:
     def __init__(self, name):
         self.name = name
-        self.pet_list = []
 
-
-    def get_pets(self):
-        owner_pets = []
-        for pet in Pet.all_pets:
-            if pet.owner == self:
-            owner_pets.append(pet)
-            return owner_pets        
-
+    def pets(self):
+        return [pet for pet in Pet.all if isinstance(pet.owner, Owner) and pet.owner == self]
+    
     def add_pet(self, pet):
-        if isinstance(pet, Pet):
-            pet.owner = self 
-            self.pet_list.append(pet)
-        else:
-            raise TypeError("Only instances of the Pet class can be added as pets.")
+        if not isinstance(pet, Pet):
+            raise Exception("Invalid pet type")
+        pet.owner = self
 
     def get_sorted_pets(self):
-        sorted_pets = sorted(self.pets, key=lambda pet: pet.name)
-        return sorted_pets              
-        
-        
+        sorted_pets = sorted(self.pets(), key=lambda x: x.name)
+        return sorted_pets
